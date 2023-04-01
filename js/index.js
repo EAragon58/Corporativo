ede73ec5656922ca498504ab3138c005b2ce6d2f
EAragon58 <129625933+EAragon58@users.noreply.github.com>
2023-04-01 16:17:55 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar-brand', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+sr.reveal('.nav', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.list-group-item', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.container', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.parrafo', {
+    duration: 3000,
+    origin: 'top',
+    distance: '-100px'
+});
